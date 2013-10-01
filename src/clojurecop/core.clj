e075b23e75cf162c36ea2364e268ee99b8acc7bf
Anton Podviaznikov <podviaznikov@gmail.com>
2013-10-01 16:57:38 -0400

    start with counting number of private and public methods in a file

--- src/clojurecop/core.clj
+++ src/clojurecop/core.clj
@@ -1,6 +1,39 @@
 (ns clojurecop.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+
+(defn- wrap-list [s]
+  (str "(" s ")"))
+
+
+
+(defn read-clj-file [filepath]
+  (read-string (wrap-list (slurp filepath))))
+
+
+
+(read-clj-file "src/clojurecop/core.clj")
+
+
+
+(defn private-fn? [code-unit]
+  (= 'defn- (first code-unit)))
+
+(defn public-fn? [code-unit]
+  (= 'defn (first code-unit)))
+
+
+(defn count-private-fns [src-code]
+  (count (filter private-fn? src-code)))
+
+
+(defn count-public-fns [src-code]
+  (count (filter public-fn? src-code)))
+
+
+
+
+
+(count-private-fns (read-clj-file "src/clojurecop/core.clj"))
+
+(count-public-fns (read-clj-file "src/clojurecop/core.clj"))
