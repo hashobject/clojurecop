6247f085ed8ab31c71c0c0681dc115ee52b1e4a6
Anton Podviaznikov <podviaznikov@gmail.com>
2013-10-01 18:08:35 -0400

    progresswith counting number of private, public and documented methods

--- src/clojurecop/core.clj
+++ src/clojurecop/core.clj
@@ -7,33 +7,46 @@
 
 
 
-(defn read-clj-file [filepath]
+(defn- read-clj-file [filepath]
   (read-string (wrap-list (slurp filepath))))
-
-
-
-(read-clj-file "src/clojurecop/core.clj")
-
 
 
 (defn private-fn? [code-unit]
   (= 'defn- (first code-unit)))
 
 (defn public-fn? [code-unit]
-  (= 'defn (first code-unit)))
+  (= "defn" (str (first code-unit))))
+
+(defn fn-has-doc? [code-unit]
+  (string? (nth code-unit 2)))
+
+(defn public-fn-with-doc? [code-unit]
+  (and (public-fn? code-unit)
+       (fn-has-doc? code-unit)))
 
 
-(defn count-private-fns [src-code]
+(defn count-private-fns
+  "Count number of private fns in the src-code unit"
+  [src-code]
   (count (filter private-fn? src-code)))
 
 
-(defn count-public-fns [src-code]
+(defn count-public-fns
+  "Count number of public fns in the src-code unit"
+  [src-code]
   (count (filter public-fn? src-code)))
 
-
+(defn count-public-fns-with-doc [src-code]
+  (count (filter public-fn-with-doc? src-code)))
 
 
 
 (count-private-fns (read-clj-file "src/clojurecop/core.clj"))
 
 (count-public-fns (read-clj-file "src/clojurecop/core.clj"))
+
+(count-public-fns-with-doc (read-clj-file "src/clojurecop/core.clj"))
+
+
+
+;(public-fn? '(defn x [] 3))
